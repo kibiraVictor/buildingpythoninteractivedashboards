fd2e46a9a297038c1bee89221ffe9b0e2f2daef4
Mkatobudu <105675349+Mkatobudu@users.noreply.github.com>
2024-05-23 21:15:57 +0300

    updates

--- app_v2.py
+++ app_v2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import dash
+import plotly
 import dash_bootstrap_components as dbc
 from dash import dcc
 from dash import html
@@ -8,7 +9,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.pool import NullPool
 import plotly.graph_objects as go
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SUPERHERO])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
 
 connection_url = "postgresql+psycopg2://root:root@172.18.0.2:5432/povertydb"
 
@@ -25,14 +26,29 @@ regions = ['East Asia & Pacific', 'Europe & Central Asia', 'Fragile and conflict
 population_df = df[~df['Country Name'].isin(regions) & (df['Indicator Name'] == 'Population, total')]
 
 app.layout = html.Div([
-    html.H1('Poverty And Equity Database'),
+    html.H1('Poverty And Equity Database',
+            style = {
+                'color':'blue',
+                'fontsize':'40px'
+            }),
     html.Br(),
-
-    dcc.Dropdown(id='year_dropdown',
+    html.H2('The World Bank'),
+    html.Br(),
+    dcc.Dropdown(id='country_dropdown', options=[{ 'label': country,  'value': country } for country in population_df['Country Name']]),
+    html.Div(id='report'),
+    html.Br(),
+    dcc.Dropdown(id='year_dropdown', 
                  options=[{'label': str(year), 'value': str(year)} for year in range(1974, 2020)],
                  placeholder="Select a year"),
     dcc.Graph(id='population_chart')
 ])
+#mend this one to be the regions
+@app.callback(Output('report', 'children'),
+              Input('country_dropdown', 'value'))
+def display_selected_country(country):
+    if country is None:
+        return 'No country selected'
+    
 
 @app.callback(Output('population_chart', 'figure'),
               Input('year_dropdown', 'value'))
